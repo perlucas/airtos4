0fbba015738336ebfa76e1d9839bb16ce1315cc1
Lucas Pereyra <lpereyra@rapptrlabs.com>
2024-09-21 10:09:06 -0300

    VM version, ready

---
+++ notebooks/airtos/dqn/dqn_agent.py
@@ -0,0 +1,169 @@
+import tensorflow as tf
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+from .dueling_q_network import DuelingQNetwork
+from utils.envs import train_env_sample, get_random_train_env, eval_env
+from utils.training import get_random_policy, compute_avg_return
+
+
+class DuelingDQNAgent:
+
+    def __init__(self,
+                 shared_advantage_layers,
+                 value_layers,
+                 optimizer,
+                 replay_buffer_capacity = 100000):
+        '''Create a new DuelingDQNAgent instance
+        :param layers: tuple: The list of layers to use in the neural network
+        :param num_atoms: int: The number of atoms to use in the C51 distribution
+        :param optimizer: Optimizer: The optimizer to use for the neural network
+        :param min_q_value: float: The minimum Q-value
+        :param max_q_value: float: The maximum Q-value
+        :param n_step_update: int: The number of steps to update the target network
+        '''
+        
+        q_net = DuelingQNetwork(
+            train_env_sample.observation_spec(),
+            train_env_sample.action_spec(),
+            shared_advantage_layers=shared_advantage_layers,
+            value_layers=value_layers,
+            initializer=tf.keras.initializers.Constant(0.005))
+        
+        train_step_counter = tf.Variable(initial_value=0, dtype='int64')
+        
+        self.agent = dqn_agent.DqnAgent(
+            train_env_sample.time_step_spec(),
+            train_env_sample.action_spec(),
+            q_network=q_net,
+            optimizer=optimizer,
+            train_step_counter=train_step_counter,
+            gamma=1.0,
+            epsilon_greedy=0.1  # Exploration factor
+        )
+        
+        self.agent.initialize()
+
+        # Create replay buffer
+        self.replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            data_spec=self.agent.collect_data_spec,
+            batch_size=train_env_sample.batch_size,
+            max_length=replay_buffer_capacity)
+
+
+
+    def train_agent(self,
+                    initial_collect_steps,
+                    batch_size,
+                    log_dir,
+                    num_iterations = 1000,
+                    collect_steps_per_iteration = 200,
+                    eval_episodes = 2,
+                    iterations_per_env = 5,
+                    logging_interval = 10,
+                    evaluation_interval = 25):
+        '''Train the agent
+        :param initial_collect_steps: int: The number of initial steps to collect
+        :param batch_size: int: The batch size to use for training
+        :param log_dir: str: The directory to log the training data
+        :param num_iterations: int: The number of iterations to train the agent
+        :param collect_steps_per_iteration: int: The number of steps to collect per iteration
+        :param eval_episodes: int: The number of episodes to evaluate the agent
+        :param iterations_per_env: int: The number of iterations to change the training environment
+        :param logging_interval: int: The interval to log training data
+        :param evaluation_interval: int: The interval to evaluate the agent
+        :return: float: The final average return of the agent
+        '''
+        # ====================================== Collect Initial Data ======================================
+        # Collect initial data, using random policy
+        random_policy = get_random_policy()
+        self.replay_buffer.clear()
+        for _ in range(initial_collect_steps):
+            self._collect_step(train_env_sample, random_policy)
+
+        # Dataset generates trajectories with shape [BxTx...] where
+        dataset = self.replay_buffer.as_dataset(
+            num_parallel_calls=3,
+            sample_batch_size=batch_size,
+            num_steps=2).prefetch(3)
+
+        iterator = iter(dataset)
+        # ====================================== Training Loop ======================================
+
+        # (Optional) Optimize by wrapping some of the code in a graph using TF function.
+        self.agent.train = common.function(self.agent.train)
+
+        # Reset the train step.
+        self.agent.train_step_counter.assign(0)
+
+        # Evaluate the agent's policy once before training.
+        avg_return = compute_avg_return(eval_env, self.agent.policy, eval_episodes)
+
+        train_env = get_random_train_env()
+
+        # Tensorboard logger
+        file_writer = tf.summary.create_file_writer(f'{log_dir}/metrics')
+        file_writer.set_as_default()
+
+        # Compute average return variation and scale by importance factor
+        importance_factor = 0.1
+        importance_factor_update = 0.08
+        prev_avg_return = avg_return
+        cumulated_deltas = 0
+
+        for _ in range(num_iterations):
+            # Collect a few steps using collect_policy and save to the replay buffer.
+            for _ in range(collect_steps_per_iteration):
+                self._collect_step(train_env, self.agent.collect_policy)
+
+            # Sample a batch of data from the buffer and update the agent's network.
+            experience, unused_info = next(iterator)
+            train_loss = self.agent.train(experience)
+
+            step = self.agent.train_step_counter.numpy()
+
+            # Log information
+            if step % logging_interval == 0:
+                print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+                #tf.summary.scalar('loss2', data=train_loss.loss, step=int64(step))
+
+
+            # Evaluate agent
+            if step % evaluation_interval == 0:
+                avg_return = compute_avg_return(eval_env, self.agent.policy, eval_episodes)
+
+                # Compute average return variation and scale by importance factor
+                avg_return_delta = (avg_return - prev_avg_return) / abs(prev_avg_return) if prev_avg_return != 0 else 0
+                avg_return_delta *= importance_factor
+                prev_avg_return = avg_return
+                cumulated_deltas += avg_return_delta # Cumulate deltas
+                print('step = {0}: Average Return = {1:.2f} Average Return Delta = {2:.2f} Importance = {3:.2f}'.format(
+                    step, avg_return, avg_return_delta, importance_factor))
+                tf.summary.scalar('average return', data=avg_return, step=step)
+                tf.summary.scalar('cumulated deltas', data=cumulated_deltas, step=step)
+                importance_factor = min(importance_factor + importance_factor_update, 1) # Update importance factor
+
+
+            # Change training env
+            if step % iterations_per_env == 0:
+                train_env = get_random_train_env()
+
+        print("Finished execution!")
+
+        avg_return = compute_avg_return(eval_env, self.agent.policy, eval_episodes)
+
+        return {
+            'final_avg_return': avg_return,
+            'cumulated_deltas': cumulated_deltas
+        }
+        
+
+    def _collect_step(self, environment, policy):
+        time_step = environment.current_time_step()
+        action_step = policy.action(time_step)
+        next_time_step = environment.step(action_step.action)
+        traj = trajectory.from_transition(time_step, action_step, next_time_step)
+        # Add trajectory to the replay buffer
+        self.replay_buffer.add_batch(traj)
---
+++ notebooks/airtos/run_tunner.py
@@ -0,0 +1,121 @@
+import os
+from packaging import version
+from datetime import datetime
+
+import tensorflow as tf
+import keras_tuner as kt
+
+from dqn.dqn_agent import DuelingDQNAgent
+
+
+
+
+
+# Keep using keras-2 (tf-keras) rather than keras-3 (keras)
+os.environ['TF_USE_LEGACY_KERAS'] = '1'
+
+# Check TensorFlow version
+print("TensorFlow version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, "This notebook requires TensorFlow 2.0 or above."
+
+
+# Training and configuration parameters
+PARAM_REPLAY_BUFFER_CAPACITY = 100000
+PARAM_BATCH_SIZE = 64
+PARAM_NUM_ITERATIONS = 500
+PARAM_COLLECT_STEPS_PER_ITERATION = 200
+
+
+EXECUTION_ID = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+
+
+class AirtosHyperModel(kt.HyperModel):
+
+    def build(self, hp):
+        # Compute the number of layers for shared/advantage network
+        sa_layers_list = []
+        num_layers = hp.Choice("num_layers_sa", [6, 9, 12, 15])
+        layer_units = hp.Int("layer_units_sa", min_value=50, max_value=400, step=50)
+        for _ in range(num_layers):
+            sa_layers_list.append(layer_units)
+        SA_AGENT_LAYERS = tuple(sa_layers_list)
+
+        # Compute the number of layers for value network
+        v_layers_list = []
+        num_layers = hp.Choice("num_layers_v", [6, 9, 12, 15])
+        layer_units = hp.Int("layer_units_v", min_value=50, max_value=400, step=50)
+        for _ in range(num_layers):
+            v_layers_list.append(layer_units)
+        V_AGENT_LAYERS = tuple(v_layers_list)
+
+        # Compute optimizer learning rate
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=hp.Choice('learning_rate', [7e-5, 3e-4, 7e-4, 3e-3, 7e-3]))
+
+        agent = DuelingDQNAgent(
+            shared_advantage_layers=SA_AGENT_LAYERS,
+            value_layers=V_AGENT_LAYERS,
+            optimizer=optimizer,
+            replay_buffer_capacity=PARAM_REPLAY_BUFFER_CAPACITY
+        )
+
+        return agent
+
+
+    def run(self, hp, model, trial, *args, **kwargs):
+        agent = model
+
+        LOG_DIR = os.path.join(
+            os.path.dirname(__file__),
+            f"{EXECUTION_ID}/trial_{trial.trial_id}"
+        )
+
+        final_stats = agent.train_agent(
+            initial_collect_steps=1000,
+            batch_size=hp.Choice('batch_size', [32, 64, 128]),
+            log_dir=LOG_DIR,
+            num_iterations=PARAM_NUM_ITERATIONS,
+            collect_steps_per_iteration=PARAM_COLLECT_STEPS_PER_ITERATION
+        )
+
+        return final_stats
+
+class AirtosTunner(kt.BayesianOptimization):
+
+    def run_trial(self, trial, *args, **kwargs):
+        hp = trial.hyperparameters
+        model = self.hypermodel.build(hp)
+        return self.hypermodel.run(hp, model, trial=trial, *args, **kwargs)
+    
+
+tuner = AirtosTunner(
+    hypermodel=AirtosHyperModel(name='airtos4'),
+    objective=kt.Objective(name='cumulated_deltas', direction='max'),
+    max_trials=200,
+    max_retries_per_trial=0,
+    max_consecutive_failed_trials=3,
+    directory=os.path.join(os.path.dirname(__file__),EXECUTION_ID),
+    project_name=f'airtos4_{EXECUTION_ID}',
+    tuner_id='airtos4_tuner1',
+    overwrite=False,
+    executions_per_trial=1,
+    allow_new_entries=True,
+    tune_new_entries=True
+)
+
+tuner.search_space_summary(extended=True)
+
+tuner.search()
+
+# Get best hyperparameters
+best_hps = tuner.get_best_hyperparameters(num_trials=40)
+best_values = []
+for hp in best_hps:
+    print(hp.values)
+    best_values.append(hp.values)
+
+# Save best values to file
+best_values_file = os.path.join(os.path.dirname(__file__), f"{EXECUTION_ID}/best_values.txt")
+with open(best_values_file, 'w') as f:
+    f.write(str(best_values))
+
+print('Finished!')

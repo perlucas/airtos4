016345991a46cb4e4311f2905782dffbf606a431
Lucas Pereyra <lpereyra@rapptrlabs.com>
2024-12-06 08:57:05 -0300

    A2C optimization #1

--- notebooks/airtos/benchmark/optuna_a2c.py
+++ notebooks/airtos/benchmark/optuna_a2c.py
@@ -65,13 +65,14 @@ RUNS_PER_TRIAL = 1
 
 def objective(trial):
     
-    learning_rate = trial.suggest_loguniform("learning_rate", 3e-7, 5e-5)
+    learning_rate = trial.suggest_loguniform("learning_rate", 5e-6, 1e-4)
     
-    num_layers = trial.suggest_categorical("num_layers", [2, 4, 8, 10, 15])
-    layer_units = trial.suggest_categorical("layer_units", [25, 50, 100])
+    num_layers = trial.suggest_categorical("num_layers", [4, 6, 8, 10])
+    layer_units = trial.suggest_categorical("layer_units", [25, 50, 100, 130])
     layers_list = [layer_units] * num_layers
 
-    activation_fn = trial.suggest_categorical("activation_fn", ["ReLU", "LeakyReLU", "ELU"])
+    # activation_fn = trial.suggest_categorical("activation_fn", ["ReLU", "LeakyReLU", "ELU"])
+    activation_fn = "ELU"
 
     policy_kwargs = dict(net_arch=layers_list, activation_fn=getattr(nn, activation_fn))
 

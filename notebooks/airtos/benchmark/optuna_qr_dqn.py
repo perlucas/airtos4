c1746cca7c92ec9b307b7ce02f4b7bea56198d7d
Lucas Pereyra <lpereyra@rapptrlabs.com>
2024-12-09 09:40:26 -0300

    QR-DQN optimization #2

--- notebooks/airtos/benchmark/optuna_qr_dqn.py
+++ notebooks/airtos/benchmark/optuna_qr_dqn.py
@@ -64,10 +64,10 @@ class SwitchEnvWrapper(gymnasium.Wrapper):
 RUNS_PER_TRIAL = 1
 
 def objective(trial):    
-    learning_rate = trial.suggest_loguniform("learning_rate", 1e-6, 5e-4)
+    learning_rate = trial.suggest_loguniform("learning_rate", 8e-5, 4e-4)
     
-    num_layers = trial.suggest_categorical("num_layers", [1, 2, 3, 4, 8, 10])
-    layer_units = trial.suggest_categorical("layer_units", [25, 50, 75, 100])
+    num_layers = trial.suggest_categorical("num_layers", [1, 2, 3, 4])
+    layer_units = trial.suggest_categorical("layer_units", [50, 75, 100])
     layers_list = [layer_units] * num_layers
 
     # activation_fn = trial.suggest_categorical("activation_fn", ["ReLU", "LeakyReLU", "ELU"])

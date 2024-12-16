6572954fb41320e5c3bf126f1202f8336cda2715
root <root@ppo>
2024-12-16 11:31:55 +0000

    fix error

--- notebooks/airtos/benchmark/optuna_ppo.py
+++ notebooks/airtos/benchmark/optuna_ppo.py
@@ -65,10 +65,10 @@ RUNS_PER_TRIAL = 1
 
 def objective(trial):
     
-    learning_rate = trial.suggest_loguniform("learning_rate", 3e-5, 5e-5)
+    learning_rate = trial.suggest_loguniform("learning_rate", 5e-5, 5e-4)
     
-    num_layers = trial.suggest_categorical("num_layers", [7, 8, 9])
-    layer_units = trial.suggest_categorical("layer_units", [23, 25, 27])
+    num_layers = trial.suggest_categorical("num_layers", [4,7, 8, 9])
+    layer_units = trial.suggest_categorical("layer_units", [50, 25, 27])
     layers_list = [layer_units] * num_layers
 
     # activation_fn = trial.suggest_categorical("activation_fn", ["ReLU", "LeakyReLU", "ELU"])

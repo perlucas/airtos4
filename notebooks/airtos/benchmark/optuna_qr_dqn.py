9867217980c214fc770a104f825ad062e5dca4ed
Lucas Pereyra <lpereyra@rapptrlabs.com>
2024-12-17 08:45:57 -0300

    qr-dqn training #1

--- notebooks/airtos/benchmark/optuna_qr_dqn.py
+++ notebooks/airtos/benchmark/optuna_qr_dqn.py
@@ -66,8 +66,8 @@ RUNS_PER_TRIAL = 1
 def objective(trial):    
     learning_rate = trial.suggest_loguniform("learning_rate", 2e-4, 5e-4)
     
-    num_layers = trial.suggest_categorical("num_layers", [1, 2])
-    layer_units = trial.suggest_categorical("layer_units", [80, 85, 90])
+    num_layers = trial.suggest_categorical("num_layers", [1, 2, 4, 6])
+    layer_units = trial.suggest_categorical("layer_units", [60, 80, 100])
     layers_list = [layer_units] * num_layers
 
     # activation_fn = trial.suggest_categorical("activation_fn", ["ReLU", "LeakyReLU", "ELU"])
